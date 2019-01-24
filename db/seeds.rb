751ee8acc359b9073d8493ee50f25a1656e3e375
Jessica Poe <jesspoe00@gmail.com>
2019-01-24 10:51:56 -0800

    added item and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(firstname: "Maggie", lastname: "Hall", email: "user1@gmail.com", password: "first", password_confirmation: "first", phone: "407-639-2351", address: "200 Yellow Brick Road", address_2: "Unit 123", city: "Renton", state: "Washington", zip: "98101")
+Item.create(user: user1, name: "longboard", category: "wheels", price: 10.00, image_path: "images/longboard.jpg", description: "Ron Jon's Surf Shop board")
